40bd3647046cb4c5ce141a4603bd025b492be721
Magesh Kuppan <tkmagesh77@gmail.com>
2020-06-13 15:49:51 +0530

    calculator web app implementation

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -2,37 +2,61 @@ const http = require('http'),
     fs = require('fs'),
     path = require('path'),
     url = require('url'),
-    querystring = require('querystring');
+    querystring = require('querystring')
+    chalk = require('chalk'),
+    calculator = require('./calculator');
 
-/* 
-req - IncomingMessage -> ReadableStream
-res - ServerResponse -> WritableStream
-*/
-/* const server = http.createServer((req, res) => {
-    res.write('<h1>Welcome to Node.js</h1>');
-    res.end();
-}); */
-console.log(__dirname);
+var staticResExtns = ['.html', '.css', '.js', '.xml', '.json', '.png', '.ico', '.txt'];
+
+function isStatic(resource){
+    var resourceExtn = path.extname(resource);
+    return staticResExtns.indexOf(resourceExtn) >= 0 ;
+}
 
 const server = http.createServer((req, res) => {
     const urlObj = url.parse(req.url);
-    console.log(querystring.parse(urlObj.query));
-    
+    console.log(chalk.yellow(req.method) + '\t' + chalk.green(urlObj.pathname));
+
     const resourceName = urlObj.pathname === '/' ? 'index.html' : urlObj.pathname;
-    const resourceFullName = path.join(__dirname, resourceName);
-    if (!fs.existsSync(resourceFullName)) {
+    if (isStatic(resourceName)){
+        const resourceFullName = path.join(__dirname, resourceName);
+        if (!fs.existsSync(resourceFullName)) {
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+        const stream = fs.createReadStream(resourceFullName);
+        stream.pipe(res);
+    } else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
+        var queryData = querystring.parse(urlObj.query),
+            x = parseInt(queryData.x),
+            y = parseInt(queryData.y),
+            op = queryData.op,
+            result = calculator[op](x,y);
+
+        res.write(result.toString());
+        res.end();
+    } else if (urlObj.pathname === '/calculator' && req.method === 'POST') {
+        var rawData = '';
+        req.on('data', chunk => rawData += chunk );
+        req.on('end', () => {
+            var bodyData = querystring.parse(rawData),
+                x = parseInt(bodyData.x),
+                y = parseInt(bodyData.y),
+                op = bodyData.op,
+                result = calculator[op](x, y);
+
+            res.write(result.toString());
+            res.end();
+        });
+    } else {
         res.statusCode = 404;
         res.end();
-        return;
     }
-    const stream = fs.createReadStream(resourceFullName);
-    /* stream.on('data', chunk => res.write(chunk));
-    stream.on('end', () => res.end()); */
-    stream.pipe(res);
 });
 
 server.listen(8080);
 
 server.on('listening', () => {
-    console.log('Server listening on port 8080');
+    console.log(chalk.cyan('Server listening on port 8080'));
 });
